c7cd0856fcaf32db44a9f77089bcd2ee422570c1
Dahlia Amadé <dahliaamade1@gmail.com>
2020-04-08 18:59:18 -0400

    Algorithm Exercises Uploaded

---
+++ 03-Algorithm_MW/01-character-count/Unsolved/character-count.js
@@ -0,0 +1,8 @@
+// Write code to create a function that accepts a string and returns an object containing the number of times each character appears in the string
+
+var characterCount = function(str) {
+    var str = "Hello World!"
+    console.log(str);
+};
+
+characterCount();
